8349a537dab108a8425e1cab660865ee3997c319
Robel <robi_ber@hotmail.com>
2020-05-13 14:26:58 +0200

    Updates

---
+++ precache-manifest.186a8d5b68a1540c87b65d783249f142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789175780ea453ed7eba112e3c11d667",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "e213902f5e8742de3260",
+    "url": "/covid19/static/css/main.ac29b173.chunk.css"
+  },
+  {
+    "revision": "477e38424f784b31a4df",
+    "url": "/covid19/static/js/2.9312463b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.9312463b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e213902f5e8742de3260",
+    "url": "/covid19/static/js/main.423d44a1.chunk.js"
+  },
+  {
+    "revision": "096422a7c9ef5cf0523f",
+    "url": "/covid19/static/js/runtime-main.076d3c7a.js"
+  },
+  {
+    "revision": "8d35201b872ed691b4fbbc004b55e8a2",
+    "url": "/covid19/static/media/image.8d35201b.png"
+  }
+]);
